c1aa00b7f8b66877cff83e622087787dc5141825
Emerson Cloud <emerson.cloud@gmail.com>
2023-12-01 15:52:16 -0500

    Remove unused files

--- scripts/register.ts
+++ scripts/register.ts
@@ -4,9 +4,8 @@
 
 import { createWalletClient, http } from "https://esm.sh/viem@1.6.4";
 import { mnemonicToAccount } from "https://esm.sh/viem@1.6.4/accounts";
-import { chains } from "http://localhost:8000/js/chains.js";
 const abi = await fetch("http://localhost:8000/js/abi.json").then((res) =>
-  res.json()
+  res.json(),
 );
 
 const fork = {
@@ -46,14 +45,13 @@ const client = createWalletClient({
   transport: http(chain.rpcUrls.public.http[0]),
 });
 
-// TODO get root from contract instead
 console.log(`Register ${addrToRegister} with sig ${sigToVerify}`);
 let resp = await fetch(`${pandasiaUrl}/trees`);
 const trees = await resp.json();
 const root = trees[0].Root;
 
 resp = await fetch(
-  `${pandasiaUrl}/proof/${root}?addr=${addrToRegister}&sig=${sigToVerify}`
+  `${pandasiaUrl}/proof/${root}?addr=${addrToRegister}&sig=${sigToVerify}`,
 );
 const proof = await resp.json();
 console.log(proof);
